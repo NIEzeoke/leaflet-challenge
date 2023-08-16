4d34a0c11940e496fa87c22157e2d54f07fa4d4c
Nnamdi <nnamdi.ezeoke@gmail.com>
2023-08-16 12:14:52 -0700

    working markers

--- Leaflet-part-1/static/js/leaflet_logic.js
+++ Leaflet-part-1/static/js/leaflet_logic.js
@@ -1,47 +1,58 @@
-function createMap(quakeEvents) {
-  // Tile layer that will be the background of our map.
-  let streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  });
+// Tile layer that will be the background of our map.
+let streetmap = L.tileLayer(
+  "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+  {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }
+);
 
-  //baseMaps object to hold the streetmap layer.
-  let baseMaps = {
-    "Street Map": streetmap
-  };
+//baseMaps object to hold the streetmap layer.
+let baseMaps = {
+  "Street Map": streetmap,
+};
+// Layer group
+let quakeEvents = new L.LayerGroup();
 
-  // overlayMaps object to hold the earthquake event layer.
-  let overlayMaps = {
-    "Earthquake Events": quakeEvents
-  };
-  
-  //Map object with options.
-  let map = L.map("map-id", {
-    center: [40.73, -74.0059],
-    zoom: 12,
-    layers: [streetmap, quakeEvents]
-  });
+// overlayMaps object to hold the earthquake event layer.
+let overlayMaps = {
+  "Earthquake Events": quakeEvents,
+};
 
-  //Layer control
-  L.control.layers(baseMaps, overlayMaps, { 
-    collapsed: false
-  }).addTo(map);
+//Map object with options.
+let map = L.map("map-id", {
+  center: [40.73, -74.0059],
+  zoom: 3,
+  layers: [streetmap],
+});
+
+//Layer control
+L.control
+  .layers(baseMaps, overlayMaps, {
+    collapsed: false,
+  })
+  .addTo(map);
+
+d3.json(
+  "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+).then(function (data) {
+  console.log(data);
+
+function styleInfo(styling) {
+  return {
+    color: 'red',
+    fillColor: 'blue',
+    radius: 6
+  }
 }
 
+  L.geoJson(data, {
+    pointToLayer: function (feature, coords) {
+      return L.circleMarker(coords);
+    },
+    style: styleInfo
+  }).addTo(quakeEvents);
 
-// THE FOLLOWING FUNCTION NEEDS COMPLETION
-//*************** */
-function createMarkers(response) {
-    // Assign variable to features key in JSON, 
-    // let features = response.features[0];
+  quakeEvents.addTo(map);
+});
 
-    // Array to hold quake markers
-    let quakeMarkers = [];
-
-    //Loop through quake array
-    // for (i =0; i <features.length; i++)
-    
-}
-
-let response = d3.json('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson')
-console.log(response.data.features)
-// console.log(data)
